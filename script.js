effcd4b76fa8ff3864056e51e98e437623257509
mujha1017-debug <mujha1017@gmail.com>
2026-05-25 14:58:03 +0700

    Refactor event handling for search and cart interactions

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+// Search form
+let searchForm = document.querySelector(".search-form");
+
+// Tombol search
+let searchBtn = document.querySelector("#search-btn");
+
+// Cart
+let cartItem = document.querySelector(".cart-items-container");
+
+// Tombol cart
+let cartBtn = document.querySelector("#cart-btn");
+
 // Mengambil navbar
 let navbar = document.querySelector(".navbar");
 // Mengambil tombol hamburger
 let menuBtn = document.querySelector("#menu-btn");
+
+// Saat cart diklik
+cartBtn.onclick = (e) => {
+  e.stopPropagation();
+
+  cartItem.classList.toggle("active");
+
+  // Tutup search
+  searchForm.classList.remove("active");
+
+  // Tutup navbar
+  navbar.classList.remove("active");
+};
 
 // Saat hamburger diklik
 menuBtn.onclick = (e) => {
@@ -11,11 +36,32 @@ menuBtn.onclick = (e) => {
   navbar.classList.toggle("active");
 };
 
+// Saat search diklik
+searchBtn.onclick = (e) => {
+  e.stopPropagation();
+
+  searchForm.classList.toggle("active");
+
+  // Tutup cart
+  cartItem.classList.remove("active");
+
+  // Tutup navbar
+  navbar.classList.remove("active");
+};
+
 // Saat Klik di luar navbar
 document.addEventListener("click", function (e) {
   // Jika klik bukan navbar dan bukan tombol menu
   if (!navbar.contains(e.target) && !menuBtn.contains(e.target)) {
     navbar.classList.remove("active");
+  } // Tutup search saat klik luar
+  if (!searchForm.contains(e.target) && !searchBtn.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  // Tutup cart saat klik luar
+  if (!cartItem.contains(e.target) && !cartBtn.contains(e.target)) {
+    cartItem.classList.remove("active");
   }
 });
 
