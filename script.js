fc9f48aa43318d98b8b1d8aef0cf09bd11c3d363
mujha1017-debug <mujha1017@gmail.com>
2026-05-25 17:02:52 +0700

    Refactor comments and improve code structure

--- script.js
+++ script.js
@@ -1,71 +1,95 @@
-// Search form
+// =======================
+// SEARCH FORM
+// =======================
+
 let searchForm = document.querySelector(".search-form");
 
-// Tombol search
 let searchBtn = document.querySelector("#search-btn");
 
-// Cart
+// =======================
+// SHOPPING CART
+// =======================
+
 let cartItem = document.querySelector(".cart-items-container");
 
-// Tombol cart
 let cartBtn = document.querySelector("#cart-btn");
 
-// Mengambil navbar
+// =======================
+// NAVBAR
+// =======================
+
 let navbar = document.querySelector(".navbar");
-// Mengambil tombol hamburger
+
 let menuBtn = document.querySelector("#menu-btn");
 
-// Saat cart diklik
+// =======================
+// CART CLICK
+// =======================
+
 cartBtn.onclick = (e) => {
   e.stopPropagation();
 
   cartItem.classList.toggle("active");
 
-  // Tutup search
   searchForm.classList.remove("active");
 
-  // Tutup navbar
   navbar.classList.remove("active");
 };
 
-// Saat hamburger diklik
+// =======================
+// MENU CLICK
+// =======================
+
 menuBtn.onclick = (e) => {
-  // Mencegah bug klik
   e.stopPropagation();
-  // Tambah/hapus class active
+
   navbar.classList.toggle("active");
+
+  searchForm.classList.remove("active");
+
+  cartItem.classList.remove("active");
 };
 
-// Saat search diklik
+// =======================
+// SEARCH CLICK
+// =======================
+
 searchBtn.onclick = (e) => {
   e.stopPropagation();
 
   searchForm.classList.toggle("active");
 
-  // Tutup cart
-  cartItem.classList.remove("active");
-
-  // Tutup navbar
   navbar.classList.remove("active");
+
+  cartItem.classList.remove("active");
 };
 
-// Saat Klik di luar navbar
+// =======================
+// KLIK LUAR
+// =======================
+
 document.addEventListener("click", function (e) {
-  // Jika klik bukan navbar dan bukan tombol menu
   if (!navbar.contains(e.target) && !menuBtn.contains(e.target)) {
     navbar.classList.remove("active");
-  } // Tutup search saat klik luar
+  }
+
   if (!searchForm.contains(e.target) && !searchBtn.contains(e.target)) {
     searchForm.classList.remove("active");
   }
 
-  // Tutup cart saat klik luar
   if (!cartItem.contains(e.target) && !cartBtn.contains(e.target)) {
     cartItem.classList.remove("active");
   }
 });
 
-// Scroll otomatis tutup
+// =======================
+// SCROLL
+// =======================
+
 window.onscroll = () => {
   navbar.classList.remove("active");
+
+  searchForm.classList.remove("active");
+
+  cartItem.classList.remove("active");
 };
